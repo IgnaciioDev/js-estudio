0e76cda9e8cf83344df2882fe46544fc59309fa2
Ignacio <ignacio9781@gmail.com>
2023-06-20 19:01:17 -0400

    practicando operadores

---
+++ 10-Estructuras/js/01-app.js
@@ -0,0 +1,20 @@
+// Ejecutar el codigo fase a una condicion
+
+const puntaje = 1000;
+
+// () condicion, == comparar, === comparador estricto, != diferente,
+// if(puntaje != 1000 ) {
+//     console.log('Si es diferente');
+// }
+
+if(puntaje === '1000' ) {
+    console.log('Si es igual');
+}else {
+    console.log('No es igual');
+}
+
+if(puntaje !== '1000' ) {
+    console.log('Si es diferente');
+}else {
+    console.log('No es diferente');
+}
