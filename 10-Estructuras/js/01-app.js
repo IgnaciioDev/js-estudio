933eb33f6d2a37e90525c42b42c0c2a538cd8d9b
Ignacio <ignacio9781@gmail.com>
2023-06-20 22:34:02 -0400

    practicando if y else

--- 10-Estructuras/js/01-app.js
+++ 10-Estructuras/js/01-app.js
@@ -1,6 +1,9 @@
 // Ejecutar el codigo fase a una condicion
 
 const puntaje = 1000;
+const totalAPagar = 500;
+const tarjeta = true;
+const dinero = 300;
 
 // () condicion, == comparar, === comparador estricto, != diferente,
 // if(puntaje != 1000 ) {
@@ -17,4 +20,13 @@ if(puntaje !== '1000' ) {
     console.log('Si es diferente');
 }else {
     console.log('No es diferente');
+}
+
+
+if(dinero >= totalAPagar) {
+    console.log('Si podemos pagar');
+} else if(tarjeta) {
+    console.log('Si puedo pagar porque tengo la tarjeta');
+}else {
+    console.log('Fondos Insuficientes');
 }
