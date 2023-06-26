e8f890c3a77efd7413a4307d3d0c97b0ada37dcd
Ignacio <ignacio9781@gmail.com>
2023-06-26 06:31:05 -0400

    practicando while loop

---
+++ 11-Iteradores/js/04-app.js
@@ -0,0 +1,12 @@
+// white loop
+// Un white se va ejecutar hasta que una condicion sea verdadera
+
+let i = 0; // Inicializar el While
+
+while(i < 10) { // Condicion
+    console.log(`Numero ${i}`)
+
+    i++ // Incremento
+}
+
+
