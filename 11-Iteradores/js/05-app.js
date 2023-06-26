215704035678a8e885717da7d777ee5e87bc1132
Ignacio <ignacio9781@gmail.com>
2023-06-26 05:43:30 -0400

    do while loop

---
+++ 11-Iteradores/js/05-app.js
@@ -0,0 +1,11 @@
+// do white se ejecuta al menos una vez y verifica la condicion
+// que se cumpla o no
+
+let i = 0;
+do {
+
+    console.log(`Numero${i}`);
+
+
+    i++ // Incremento
+} while(i < 10); // Condicion
