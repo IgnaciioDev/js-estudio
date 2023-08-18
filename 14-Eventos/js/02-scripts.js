1384900d3647202734cb2bfde31be0eea58f7f38
Ignacio Castro <ignacio9781@gmail.com>
2023-08-18 02:56:12 -0400

    evento con el mouse

---
+++ 14-Eventos/js/02-scripts.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector('.navegacion');
+
+// Registrar un evento
+
+nav.addEventListener('mouseenter', () => {
+    console.log('entrando en la navegacion');
+})
+
+
+// mousedown - similar al click
+// click
+// dbclick = doble click
+// mouseup - cuando suelta el mouse
+//mouseout
+
+// aqui son eventos con el mouse
