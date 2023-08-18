3322b587e24d0da7cec4a3973afeb7a1a46a7749
Ignacio Castro <ignacio9781@gmail.com>
2023-08-18 02:54:51 -0400

    detectar el html

---
+++ 14-Eventos/js/01-scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Documento Listo');
+});
+
+
+// DOMContentLoaded
+// Esto se ejecuta cuando todo el elemento de html sea cargado
+
+
+// Esto es para detectar que el html esta listo
